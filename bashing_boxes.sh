47bae10674f4d4c7de62db9f846b68f049d8a783
Bayo12JaMorant <bayoibironke1@outlook.com>
2025-10-15 10:49:59 -0500

    print item tbc

--- bashing_boxes.sh
+++ bashing_boxes.sh
@@ -1,53 +1,43 @@
 #!/bin/bash
+
+
 objectGenerator=( "Puzzle" "Saw" "Surfboard" "Holly" "AvocadoTree" "Scanner" "Typewriter" "Nightstand" "Mint" "TapeMeasure" )
-echo "${objectGenerator[@]}"
 
-forPuzzle()
+print_List()
 {
+  echo "The list of items is..."
+  sleep 2
+  echo "${objectGenerator[@]}"
+}
+print_list
 
+print_Item()
+{
+   sleep 1
+   echo "What Item would you like to pick?"
+  for object in "${objectGenerator[@]}"; do
+   echo "Your item is: $object"
+  done	
+
+  
 }
 
-forSaw()
+add_Item()
 {
-
+  sleep 1
 }
 
-forSurfboard()
+remove_Last_Item()
 {
-
+  sleep 1
 }
 
-forHolly()
+remove_Item()
 {
-
+  sleep 1
 }
 
-forAvocadoTree()
+exit_Function()
 {
-   
-}
-
-forScanner()
-{
-
-}
-
-forTypewriter()
-{
-
-}
-
-forNightstand()
-{
-
-}
-
-forMint()
-{
-
-}
-
-forTapeMeasure()
-{
-
+  sleep 1
 }
