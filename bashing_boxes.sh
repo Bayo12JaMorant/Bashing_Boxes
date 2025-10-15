2066381b52df209bd2f5a903d46567e6548b76e0
Bayo12JaMorant <bayoibironke1@outlook.com>
2025-10-15 15:24:44 -0500

    tourbleshooting conitnued

--- bashing_boxes.sh
+++ bashing_boxes.sh
@@ -2,42 +2,46 @@
 
 
 objectGenerator=( "Puzzle" "Saw" "Surfboard" "Holly" "AvocadoTree" "Scanner" "Typewriter" "Nightstand" "Mint" "TapeMeasure" )
+  
 
-print_List()
-{
   echo "The list of items is..."
   sleep 2
   echo "${objectGenerator[@]}"
-}
-print_list
+  
+  print_List()
+  {
+  echo "The list of items is..."
+  sleep 2
+  echo "${objectGenerator[@]}"
+  }
 
-print_Item()
-{
+  print_list
+
+  print_Item()
+  {
    sleep 1
-   echo "What Item would you like to pick?"
+   read -p "What Item would you like to pick?:" answer
   for object in "${objectGenerator[@]}"; do
    echo "Your item is: $object"
   done	
+  }
 
-  
-}
+  add_Item()
+  {
+    sleep 1
+  }
 
-add_Item()
-{
-  sleep 1
-}
+  remove_Last_Item()
+  {
+    sleep 1
+  }
 
-remove_Last_Item()
-{
-  sleep 1
-}
+  remove_Item()
+  {
+    sleep 1
+  }
 
-remove_Item()
-{
-  sleep 1
-}
-
-exit_Function()
-{
-  sleep 1
-}
+  exit_Function()
+  {
+    sleep 1
+  }
