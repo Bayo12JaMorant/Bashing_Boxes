2fa9e84f113453b56d318150ad0ff87b65ebf070
Bayo12JaMorant <bayoibironke1@outlook.com>
2025-10-16 15:28:53 -0500

    CHANGE remove_Item

--- bashing_boxes.sh
+++ bashing_boxes.sh
@@ -7,34 +7,43 @@ objectGenerator=( "Puzzle" "Saw" "Surfboard" "Holly" "AvocadoTree" "Scanner" "Ty
   print_List()
   {
   echo "The list of items is..."
-  sleep 2
+  sleep 1
   echo "${objectGenerator[@]}"
   }
 
- 
+
 
   print_Item()
   {
    sleep 1
-   read -p "What Item would you like to pick?:" answer
-  for object in "${objectGenerator[@]}"; do
-   echo "Your item is: $object"
-  done	
+   read -p "What Item would you like to pick? (0-9):" answer
+   echo "Your item is: ${objectGenerator[$answer]}"
+ 	
   }
 
   add_Item()
   {
     sleep 1
+    read -p "What is the name of the object you want to add?:" answer
+    objectGenerator+=( $answer )
+    echo "${objectGenerator[@]}"
+
   }
 
   remove_Last_Item()
   {
     sleep 1
+    unset 'objectGenerator[9]'
+    echo "The list of items is now... "
+    echo "${objectGenerator[@]}"
   }
 
   remove_Item()
   {
     sleep 1
+    read -p "Which object would you like to remove?(0-9):" answer
+    unset 'objectGenerator'
+    echo "${objectGenerator[@]}"
   }
 
  
@@ -43,7 +52,15 @@ objectGenerator=( "Puzzle" "Saw" "Surfboard" "Holly" "AvocadoTree" "Scanner" "Ty
     echo "The list of items is..."
     sleep 2
     echo "${objectGenerator[@]}"
+    #sleep 2
+    #echo "${objectGenerator[4]}"
+    #sleep 2
+    #echo "${objectGenerator[8]}"
+    #sleep 2
+    #echo "${objectGenerator[0]}"
     sleep 2
+
+    #print_Item
   
 
     echo "Full List of objects; 1"
@@ -55,28 +72,27 @@ objectGenerator=( "Puzzle" "Saw" "Surfboard" "Holly" "AvocadoTree" "Scanner" "Ty
 
     read -p "Which option would you like to choose?(1-6):" answer
   if [[ $answer == "1" ]];then
-    sleep 2
-    print_list
+    print_List
   elif [[ $answer == "2" ]];then
-    sleep 2
     print_Item
   elif [[ $answer == "3" ]];then
-    sleep 2
     add_Item
   elif [[ $answer == "4" ]];then
-    sleep 2
     remove_Last_Item
   elif [[ $answer == "5" ]];then
-    sleep 2
+    #sleep 2
     remove_Item
   elif [[ $answer == "6" ]];then
     echo "Thanks for looking through the objects!"
-    sleep 2
+    sleep 1
     exit
   fi  
-  s
+  
 
           
 
 
 
+ #for object in "${objectGenerator[@]}"; do
+  # echo "Your item is: $object"
+  #done
