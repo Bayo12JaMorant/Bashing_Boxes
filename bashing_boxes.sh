e94f0860af1f9c48d9068348cdaa9b8bb7278899
Bayo12JaMorant <bayoibironke1@outlook.com>
2025-10-16 10:21:12 -0500

    options added

--- bashing_boxes.sh
+++ bashing_boxes.sh
@@ -3,10 +3,6 @@
 
 objectGenerator=( "Puzzle" "Saw" "Surfboard" "Holly" "AvocadoTree" "Scanner" "Typewriter" "Nightstand" "Mint" "TapeMeasure" )
   
-
-  echo "The list of items is..."
-  sleep 2
-  echo "${objectGenerator[@]}"
   
   print_List()
   {
@@ -15,7 +11,7 @@ objectGenerator=( "Puzzle" "Saw" "Surfboard" "Holly" "AvocadoTree" "Scanner" "Ty
   echo "${objectGenerator[@]}"
   }
 
-  print_list
+ 
 
   print_Item()
   {
@@ -41,7 +37,46 @@ objectGenerator=( "Puzzle" "Saw" "Surfboard" "Holly" "AvocadoTree" "Scanner" "Ty
     sleep 1
   }
 
-  exit_Function()
-  {
-    sleep 1
-  }
+ 
+
+
+    echo "The list of items is..."
+    sleep 2
+    echo "${objectGenerator[@]}"
+    sleep 2
+  
+
+    echo "Full List of objects; 1"
+    echo "Specific object at certain position; 2"
+    echo "Adding a new item to the list; 3"
+    echo "Remove the final item on the list; 4"
+    echo "Remove a specific item from the list; 5"
+    echo "Exit; 6"
+
+    read -p "Which option would you like to choose?(1-6):" answer
+  if [[ $answer == "1" ]];then
+    sleep 2
+    print_list
+  elif [[ $answer == "2" ]];then
+    sleep 2
+    print_Item
+  elif [[ $answer == "3" ]];then
+    sleep 2
+    add_Item
+  elif [[ $answer == "4" ]];then
+    sleep 2
+    remove_Last_Item
+  elif [[ $answer == "5" ]];then
+    sleep 2
+    remove_Item
+  elif [[ $answer == "6" ]];then
+    echo "Thanks for looking through the objects!"
+    sleep 2
+    exit
+  fi  
+  s
+
+          
+
+
+
