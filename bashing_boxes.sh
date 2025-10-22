cd95eab3d6d77e4cb9f5c5152e3ae0ccefbd2e3c
Bayo12JaMorant <bayoibironke1@outlook.com>
2025-10-22 10:50:39 -0500

    testing betwwen case;esac and if;elif

--- bashing_boxes.sh
+++ bashing_boxes.sh
@@ -3,38 +3,36 @@
 
 objectGenerator=( "Puzzle" "Saw" "Surfboard" "Holly" "AvocadoTree" "Scanner" "Typewriter" "Nightstand" "Mint" "TapeMeasure" )
   
-  
+while true; do  
   print_List()
   {
-  echo "The list of items is..."
-  sleep 1
-  echo "${objectGenerator[@]}"
+    echo "The list of items is..."
+      sleep 1
+      echo "${objectGenerator[@]}"
   }
 
 
 
   print_Item()
   {
-   sleep 1
+
    echo ""
    read -p "What Item would you like to pick? (0-9):" answer
    echo "Your item is: ${objectGenerator[$answer]}"
- 	
   }
 
   add_Item()
   {
-    sleep 1
-    echo ""
-    read -p "What is the name of the object you want to add?:" answer
-    objectGenerator+=( $answer )
-    echo "${objectGenerator[@]}"
 
+   echo ""
+   read -p "What is the name of the object you want to add?:" answer
+   objectGenerator+=( $answer )
+   echo "${objectGenerator[@]}"
   }
 
   remove_Last_Item()
   {
-    sleep 1
+      
     echo ""
     unset 'objectGenerator[9]'
     echo "The list of items is now... "
@@ -43,8 +41,8 @@ objectGenerator=( "Puzzle" "Saw" "Surfboard" "Holly" "AvocadoTree" "Scanner" "Ty
 
   remove_Item()
   {
-    sleep 1
-    echo ""
+
+   echo ""
     read -p "Which object would you like to remove?(0-9):" answer
     if [[ $answer -ge 0 && $answer -lt ${#objectGenerator[@]} ]]; then
       unset 'objectGenerator[answer]'
@@ -52,53 +50,67 @@ objectGenerator=( "Puzzle" "Saw" "Surfboard" "Holly" "AvocadoTree" "Scanner" "Ty
     fi  
   }
 
- 
+   
 
 
-    echo "The list of items is..."
-    sleep 2
-    echo "${objectGenerator[@]}"
-    #sleep 2
-    #echo "${objectGenerator[4]}"
-    #sleep 2
-    #echo "${objectGenerator[8]}"
-    #sleep 2
-    #echo "${objectGenerator[0]}"
-    sleep 2
+  echo "The list of items is..."
+  sleep 2
+  echo "${objectGenerator[@]}"
+  #sleep 2
+  #echo "${objectGenerator[4]}"
+  #sleep 2
+  #echo "${objectGenerator[8]}"
+  #sleep 2
+  #echo "${objectGenerator[0]}"
+  sleep 2
 
-    #print_Item
-  
+  #print_Item
+    
 
-    echo "Full List of objects; 1"
-    echo "Specific object at certain position; 2"
-    echo "Adding a new item to the list; 3"
-    echo "Remove the final item on the list; 4"
-    echo "Remove a specific item from the list; 5"
-    echo "Exit; 6"
+  echo "Full List of objects; 1"
+  echo "Specific object at certain position; 2"
+  echo "Adding a new item to the list; 3"
+  echo "Remove the final item on the list; 4"
+  echo "Remove a specific item from the list; 5"
+  echo "Exit; 6"
 
-    read -p "Which option would you like to choose?(1-6):" answer
-  if [[ $answer == "1" ]];then
-    print_List
-  elif [[ $answer == "2" ]];then
-    print_Item
-  elif [[ $answer == "3" ]];then
-    add_Item
-  elif [[ $answer == "4" ]];then
-    remove_Last_Item
-  elif [[ $answer == "5" ]];then
-    #sleep 2
-    remove_Item
-  elif [[ $answer == "6" ]];then
-    echo "Thanks for looking through the objects!"
-    sleep 1
-    exit
-  fi  
-  
+  read -p "Which option would you like to choose?(1-6): " answer
 
-          
+  case $answer in
+    1) print_List ;;
+    2) print_Item ;;
+    3) add_Item ;;
+    4) remove_Last_Item ;;
+    5) remove_Item ;;
+    6)
+      echo "Thanks for looking through the objects! "
+      exit ;;
+  esac
+done
 
 
 
- #for object in "${objectGenerator[@]}"; do
-  # echo "Your item is: $object"
-  #done
+
+
+
+#for object in "${objectGenerator[@]}"; do
+    # echo "Your item is: $object"
+    #done
+
+#      read -p "Which option would you like to choose?(1-6):" answer
+ ##
+ #    print_List
+  #  elif [[ $answer == "2" ]];then
+   #   print_Item
+    #elif [[ $answer == "3" ]];then
+  #    add_Item
+   # elif [[ $answer == "4" ]];then
+    #  remove_Last_Item
+    #elif [[ $answer == "5" ]];then
+      #sleep 2
+     # remove_Item
+    #elif [[ $answer == "6" ]];then
+     # echo "Thanks for looking through the objects!"
+      #sleep 1
+      #exit
+    #fi 
--- freeCode.sh
+++ freeCode.sh
@@ -9,3 +9,7 @@ echo -e "\n ${objectGenerator[5]}"
 echo -e "\n ${objectGenerator[2]}"
 echo -e "\n ${objectGenerator[@]}"
 echo -e "\n ${#objectGenerator[@]}"
+
+for (( i = 9; i < $objectGenerator; i++ )); do
+	echo -e "The amount of objects is ${objectGenerator[@]} "
+done	
